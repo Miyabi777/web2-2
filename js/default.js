577a7faf2bc6dfb2a541e8f537aec4512c5f93d5
Miyabi777 <kmgi77@icloud.com>
2017-10-02 18:18:34 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,12 +1,1 @@
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 
-$(function(){
-    $("img.ChangePhoto").click(function(){
-        var ImgSrc = $(this).attr("src");
-        var ImgAlt = $(this).attr("alt");
-        $("img#MainPhoto").attr({src:ImgSrc,alt:ImgAlt});
-        $("img#MainPhoto").hide();
-        $("img#MainPhoto").fadeIn("slow");
-        return false;
-    });
-});
