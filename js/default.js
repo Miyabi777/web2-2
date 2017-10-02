f7108145a747327945812e50d6f4ef7485375680
Miyabi777 <kmgi77@icloud.com>
2017-10-02 18:12:48 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,12 @@
-<style>
-  button{
-    cursor:pointer;
-  }
-<style>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+
+$(function(){
+    $("img.ChangePhoto").click(function(){
+        var ImgSrc = $(this).attr("src");
+        var ImgAlt = $(this).attr("alt");
+        $("img#MainPhoto").attr({src:ImgSrc,alt:ImgAlt});
+        $("img#MainPhoto").hide();
+        $("img#MainPhoto").fadeIn("slow");
+        return false;
+    });
+});
